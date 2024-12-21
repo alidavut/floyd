7321242df522b503aec2203a30f791dcd344e486
Ali Davut <adavut@gmail.com>
2024-12-21 08:20:21 +0300

    Update some copy

--- apps/web/pages/home/partials/index.ts
+++ apps/web/pages/home/partials/index.ts
@@ -1,4 +1,3 @@
 export { Cover } from './cover';
 export { Features } from './features';
-export { UseCases } from './use-cases';
 export { GetStarted } from './get-started';
--- apps/web/pages/home/view.tsx
+++ apps/web/pages/home/view.tsx
@@ -1,13 +1,13 @@
 import { Head } from 'components';
-import { Cover, Features, GetStarted, UseCases } from './partials';
+import { Cover, Features, GetStarted } from './partials';
 
 export function HomeView() {
   return (
     <div>
       <Head
-        title="Floyd - Booking infrastructure made for developers"
-        description="Floyd provides your application with a robust suite of APIs designed to manage complicated
-          scheduling and pricing effortlessly."
+        title="Floyd - Booking mastery, made for frontiers"
+        description="Floyd provides a booking system for events and experiences. Accept bookings, manage schedules,
+          and monetize events in minutes."
         titlePure
       />
       <Cover />

1700155c2247099377ebd5c76a623e971b67939d
Ali Davut <adavut@gmail.com>
2024-12-30 22:19:33 +0300

    Update get-started.tsx

--- apps/web/pages/home/partials/get-started.tsx
+++ apps/web/pages/home/partials/get-started.tsx
@@ -7,12 +7,14 @@ export function GetStarted() {
         <div className="pb-20 lg:pb-32">
           <div className="text-center">
             <h2 className="text-3xl max-w-xl mx-auto mb-3 leading-10">
-              <span className="block text-[1.375rem] font-light">Let's make your profile</span>
-              <span className="text-purple-700 text-[2.25rem] font-semibold tracking-tight">
-                simple, amazing, inspiring
+              <span className="block text-[1.25rem] font-light leading-none mb-1.5">
+                Ready to begin?
+              </span>
+              <span className="text-purple-700 text-[2.5rem] font-semibold tracking-tighter">
+                your story starts here
               </span>
             </h2>
-            <p className="text- text-bunker-400 max-w-xs mx-auto">
+            <p className="text- text-bunker-500 max-w-xs mx-auto">
               Get started with Floyd and start accepting bookings in minutes.
             </p>
             <div className="mt-9">

74a62c8a7e599f7c10e743fba475fa1a3b5b1659
Ali Davut <adavut@gmail.com>
2025-01-11 15:37:24 +0300

    Fix for mobile

--- apps/web/pages/home/partials/features.tsx
+++ apps/web/pages/home/partials/features.tsx
@@ -41,7 +41,7 @@ const cards = [
 
 export function Features() {
   return (
-    <div className="relative py-48 mb-32 text-white">
+    <div className="relative py-32 xl:py-48 mb-32 text-white">
       <div
         className="bg-bunker-950 -skew-y-6 absolute inset-0 bottom-1/3 border-t-2 border-dashed border-white"
       />
@@ -52,25 +52,25 @@ export function Features() {
         className="bg-bunker-950 skew-y-6 absolute inset-0 top-1/3"
       />
       <div className="container relative">
-        <h3 className="mb-32 text-center">
-          <span className="block font-semibold font-serif text-[4.25rem] leading-none mb-6">
-            <span className="font-sans font-extrabold uppercase text-[4rem] tracking-tight">Everything</span>{' '}
-            <span className="font-serif italic tracking-tight">you need</span>
+        <h3 className="mb-15 lg:mb-32 text-center">
+          <span className="block font-semibold font-serif leading-none mb-3 lg:mb-6 space-x-1 lg:space-x-2">
+            <span className="font-sans font-extrabold uppercase text-[2rem] lg:text-[4rem] tracking-tight">Everything</span>{' '}
+            <span className="font-serif italic tracking-tight text-[1.5rem] lg:text-[4.25rem]">you need</span>
           </span>
-          <span className="block font-semibold text-[1.675rem]">
+          <span className="block font-semibold text-[1.175rem] lg:text-[1.675rem] leading-[1.75rem] lg:leading-[2rem]">
             to <U n={1}>create</U>, <U n={2}>host</U>, and <U n={3}>monetize</U> live experiences
           </span>
         </h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-15">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 xl:gap-15">
           {cards.map((card, index) => (
             <div key={index} className="pt-24">
               <div className="relative rounded-sm">
-                <div className="absolute inset-0 border-t border-dashed border-white/20 ml-24" />
-                <div className="absolute inset-0 border-l border-dashed border-white/20 ml-56" />
-                <div className="relative flex space-x-9 z-10">
-                  <img src={card.image.src} className="rounded-sm w-48 h-48 -mt-24 -rotate-3 -skew-y-2" />
-                  <div className="flex-1 py-12 pl-9">
-                    <h4 className="text-[1.5rem] leading-[2rem] font-semibold mb-3">
+                <div className="absolute inset-0 border-t border-dashed border-white/20 xl:ml-24" />
+                <div className="absolute inset-0 border-l border-dashed border-white/20 ml-6 xl:ml-56" />
+                <div className="relative flex flex-col xl:flex-row space-x-9 z-10">
+                  <img src={card.image.src} className="rounded-sm w-36 h-36 xl:w-48 xl:h-48 -mt-24 -rotate-3 -skew-y-2" />
+                  <div className="flex-1 py-9 pl-6 pr-3 lg:py-12 lg:pl-9">
+                    <h4 className="text-[1.325rem] lg:text-[1.5rem] leading-[2rem] font-semibold mb-3">
                       {card.title}
                     </h4>
                     <p className="text-lg text-white/70">
@@ -95,11 +95,11 @@ function U({ n, children }) {
   ][n - 1];
 
   return (
-    <span className="relative">
+    <span className="relative inline-block">
       <span
-        className={cx('absolute inset-0 -skew-y-1', bg)}
+        className={cx('absolute block inset-0 top-0.5 bottom-0.5 -skew-y-2', bg)}
       />
-      <span className="relative text-bunker-950 uppercase font-semibold inline-block px-1.5 py-0.5 font-serif">
+      <span className="relative text-bunker-950 uppercase font-semibold inline-block px-0.5 font-serif">
         {children}
       </span>
     </span>

19915763a8c70a088cae97cf11b253d286202a18
Ali Davut <adavut@gmail.com>
2025-01-12 04:24:25 +0300

    Update features.tsx

--- apps/web/pages/home/partials/features.tsx
+++ apps/web/pages/home/partials/features.tsx
@@ -61,14 +61,14 @@ export function Features() {
             to <U n={1}>create</U>, <U n={2}>host</U>, and <U n={3}>monetize</U> live experiences
           </span>
         </h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 xl:gap-15">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-15">
           {cards.map((card, index) => (
             <div key={index} className="pt-24">
               <div className="relative rounded-sm">
                 <div className="absolute inset-0 border-t border-dashed border-white/20 xl:ml-24" />
                 <div className="absolute inset-0 border-l border-dashed border-white/20 ml-3 xl:ml-56" />
                 <div className="relative flex flex-col xl:flex-row space-x-9 z-10">
-                  <img src={card.image.src} className="rounded-sm w-36 h-36 xl:w-48 xl:h-48 -mt-24 -rotate-3 -skew-y-2" />
+                  <img src={card.image.src} className="rounded-sm w-48 h-48 -mt-24 -rotate-3 -skew-y-1" />
                   <div className="flex-1 pt-9 pb-6 pl-1.5 pr-3 lg:py-12 lg:pl-9">
                     <h4 className="text-[1.325rem] lg:text-[1.5rem] leading-[2rem] font-semibold mb-3">
                       {card.title}

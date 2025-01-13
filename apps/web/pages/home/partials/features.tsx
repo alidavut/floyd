9b91b45ee352b72e82c5df5b14d55e2d9a04c1a7
Ali Davut <adavut@gmail.com>
2025-01-13 09:34:56 +0300

    Update feature images

--- apps/web/pages/home/partials/features.tsx
+++ apps/web/pages/home/partials/features.tsx
@@ -68,8 +68,8 @@ export function Features() {
                 <div className="absolute inset-0 border-t border-dashed border-white/20 xl:ml-24" />
                 <div className="absolute inset-0 border-l border-dashed border-white/20 ml-3 xl:ml-56" />
                 <div className="relative flex flex-col xl:flex-row space-x-9 z-10">
-                  <img src={card.image.src} className="rounded-sm w-40 h-40 xl:w-48 xl:h-48 -mt-24 -rotate-3 -skew-y-1" />
-                  <div className="flex-1 pt-9 pb-6 pl-1.5 pr-6 lg:py-12 lg:pl-9">
+                  <img src={card.image.src} className="rounded-sm w-32 h-48 xl:w-48 xl:h-72 object-cover object-center -mt-24 -rotate-[4.5deg] -skew-y-1" />
+                  <div className="flex-1 pt-9 pb-6 pl-1.5 pr-6 lg:py-10 lg:pl-9">
                     <h4 className="text-[1.5rem] leading-[2rem] font-semibold mb-3">
                       {card.title}
                     </h4>
